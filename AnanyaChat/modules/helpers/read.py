685ad98c4bbdce0f4cbae052a112a3d2fa3ca4f6
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-09-18 15:57:58 +0530

    Create read.py

---
+++ AnanyaChat/modules/helpers/read.py
@@ -0,0 +1,65 @@
+from config import OWNER_USERNAME, SUPPORT_GRP
+from AnanyaChat import AnanyaBot
+
+START = f"""
+<b>👋 ʜᴇʏ! ɪ'ᴍ [{AnanyaBot.name}](t.me/{AnanyaBot.username})</b>  
+<b>🤖 ᴀɴ ᴀɪ-ᴘᴏᴡᴇʀᴇᴅ ᴄʜᴀᴛʙᴏᴛ 🚀</b>  
+
+<b>✨ ᴜsᴀɢᴇ: /chatbot [on/off]</b>  
+<b>🔘 ᴛᴀᴘ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏ 💡</b>  
+"""
+
+HELP_READ = f"""
+📚 <u><b>ʜᴇʟᴘ ᴍᴇɴᴜ ғᴏʀ {NoxxBot.name}</b></u>  
+
+<b>⚡ ᴜsᴇ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴡɪᴛʜ /</b>  
+
+<b>🛠️ ᴍᴀɪɴ ғᴇᴀᴛᴜʀᴇs:</b>  
+<b>- 💬 /chatbot on → ᴇɴᴀʙʟᴇ ᴀɪ ᴄʜᴀᴛ</b>  
+<b>- 🔇 /chatbot off → ᴅɪsᴀʙʟᴇ ᴀɪ ᴄʜᴀᴛ</b>  
+
+<b>👑 ᴏᴡɴᴇʀ: @{OWNER_USERNAME}</b>  
+"""
+
+TOOLS_DATA_READ = f"""
+🛠️ <u><b>ᴛᴏᴏʟs ғᴏʀ {NoxxBot.name}</b></u>  
+
+<b>📂 /repo → ɢᴇᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ</b>  
+<b>📡 /ping → ᴄʜᴇᴄᴋ ᴘɪɴɢ</b>  
+<b>🆔 /id → ɢᴇᴛ ᴜsᴇʀ ɪᴅ, ᴄʜᴀᴛ ɪᴅ & ᴍᴇssᴀɢᴇ ɪᴅ</b>  
+
+<b>👑 ᴏᴡɴᴇʀ: @{OWNER_USERNAME}</b>  
+"""
+
+CHATBOT_READ = f"""
+🤖 <u><b>ᴄʜᴀᴛʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs</b></u>  
+
+<b>💬 /chatbot on → ᴇɴᴀʙʟᴇ ᴀɪ</b>  
+<b>🔇 /chatbot off → ᴅɪsᴀʙʟᴇ ᴀɪ</b>  
+
+<b>⚠️ ɴᴏᴛᴇ: ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡᴏʀᴋs ᴏɴʟʏ ɪɴ ɢʀᴏᴜᴘs!</b>  
+
+<b>👑 ᴏᴡɴᴇʀ: @{OWNER_USERNAME}</b>  
+"""
+
+SOURCE_READ = f"""
+💻 <b>sᴏᴜʀᴄᴇ ᴏғ [{AnanyaBot.name}](https://t.me/{AnanyaBot.username})</b>  
+
+<b>🌟 ғᴏʀᴋ & sᴛᴀʀ → [GitHub Repo](https://github.com/ZOXXOP/AnanyaChat)</b>  
+
+<b>❓ ɢᴏᴛ ᴀɴʏ ɪssᴜᴇ? → [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_GRP})</b>  
+
+<b>👑 ᴏᴡɴᴇʀ: @{OWNER_USERNAME}</b>  
+"""
+
+ADMIN_READ = "<b>⚙️ ᴀᴅᴍɪɴ ғᴇᴀᴛᴜʀᴇs ᴄᴏᴍɪɴɢ sᴏᴏɴ... 🔜</b>"
+
+ABOUT_READ = f"""
+ℹ️ <u><b>ᴀʙᴏᴜᴛ {AnanyaBot.name}</b></u>  
+
+<b>🤖 ᴀɴ ᴀɪ-ʙᴀsᴇᴅ ᴄʜᴀᴛʙᴏᴛ</b>  
+<b>💬 ᴀᴜᴛᴏ-ʀᴇᴘʟɪᴇs ᴛᴏ ᴜsᴇʀs</b>  
+<b>👥 ʜᴇʟᴘs ɪɴ ɢʀᴏᴜᴘ ᴀᴄᴛɪᴠɪᴛʏ</b>  
+
+<b>👇 ᴜsᴇ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏ</b>  
+"""
