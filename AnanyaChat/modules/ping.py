8baccbbae2f6bc6d84989d1e1ae06a55af828337
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-09-18 15:32:45 +0530

    Create ping.py

---
+++ AnanyaChat/modules/ping.py
@@ -0,0 +1,37 @@
+import random
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import IMG, OWNER_USERNAME, STICKER
+from AnanyaChat import AnanyaBot
+from AnanyaChat.database.chats import add_served_chat
+from AnanyaChat.database.users import add_served_user
+from AnanyaChat.modules.helpers import PNG_BTN
+
+
+@AnanyaBot.on_cmd("ping")
+async def ping(_, message: Message):
+    await message.reply_sticker(sticker=random.choice(STICKER))
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+    try:
+        await message.delete()
+    except:
+        pass
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"нey вαву!!\n{AnanyaBot.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢](https://t.me/{OWNER_USERNAME}) ||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
+      
